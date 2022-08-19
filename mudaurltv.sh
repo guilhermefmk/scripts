c4c4eb2dcfd5a98cc9eea09f2051af84bc9355b0
Guilherme Rodrigues Cunha <80539618+guilhermefmk@users.noreply.github.com>
2022-08-19 08:39:15 -0300

    Update mudaurltv.sh

--- mudaurltv.sh
+++ mudaurltv.sh
@@ -12,39 +12,39 @@ do
     temp=$(echo $tv | cut -d"|" -f$c)
     case $temp in
     	TV1)
-            ip=172.16.114.54
+            ip="ip_aqui"
         ;;
     	TV2)
-            ip=172.16.114.22
+            ip="ip_aqui"
         ;;
         TV3)
-    		ip=172.16.114.23
+    		ip="ip_aqui"
     	;;
         TV4)
-            ip=172.16.114.11
+            ip="ip_aqui"
         ;;
         TV5)
-            ip=172.16.114.14
+            ip="ip_aqui"
         ;;
         TV6)
-            ip=172.16.114.16
+            ip="ip_aqui"
         ;;
         TV7)
-            ip=172.16.114.19
+            ip="ip_aqui"
         ;;
         TV8)
-            ip=172.16.114.117
+            ip="ip_aqui"
         ;;
         TV9)
-            ip=172.16.114.12
+            ip="ip_aqui"
         ;;
         TV10)
-            ip=172.16.114.13
+            ip="ip_aqui"
         ;;
         TV11)
-            ip=172.16.114.24
+            ip="ip_aqui"
         ;;
     esac
-    sshpass -p dietpi ssh -o ServerAliveInterval=3 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$ip "/usr/bin/sed -i '/SOFTWARE_CHROMIUM_AUTOSTART_URL=/d' /boot/dietpi.txt && sed -i '/SOFTWARE_CHROMIUM_RES_Y=/a SOFTWARE_CHROMIUM_AUTOSTART_URL='$url'' /boot/dietpi.txt && /usr/sbin/reboot"
+    sshpass -p "senha-aqui" ssh -o ServerAliveInterval=3 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@$ip "/usr/bin/sed -i '/SOFTWARE_CHROMIUM_AUTOSTART_URL=/d' /boot/dietpi.txt && sed -i '/SOFTWARE_CHROMIUM_RES_Y=/a SOFTWARE_CHROMIUM_AUTOSTART_URL='$url'' /boot/dietpi.txt && /usr/sbin/reboot"
 done
 
